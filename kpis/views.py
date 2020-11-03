d56b041b6c9d7298da95e6af378dacdb47481542
= <jason.wright90@gmail.com>
2020-11-03 09:39:46 +0000

    start work on week filter in index view

--- kpis/views.py
+++ kpis/views.py
@@ -11,33 +11,32 @@ def index(request):
     reports_partner_share = Report.objects.aggregate(Sum('partner_share'))
     reports_contribution = reports_revenue['revenue__sum'] - reports_partner_share['partner_share__sum']
 
-    reports = { }
+    yearly_reports = {}
+    for rep in reports_list:
+        if yearly_reports.get(rep.year):
+            yearly_reports[rep.year].append(rep)
+        else:
+            yearly_reports[rep.year] = [rep]
+
+    the_reports = [{"year": k, "report": v} for k, v in yearly_reports.items()]
 
 
-    for report in reports_list:
-        #print(report.year)
-        #each variable accessible with .
-        #make keys for year and weeks
-
-        week = { }
-        week[report.week_number] = { 'revenue' : report.revenue }
-
-        # Week_number as key, with value as another key/dictionary 
+    week_14_plus = []
+    week_14_customer = Report.objects.filter(week_number__gte=15)
     
-        print (week)
+    for rep in week_14_customer:
+        week_14_plus.append(rep)
+    
+    print(week_14_plus)
 
-        #reports[str(report.year)][str(report.week_number)] = { 'revenue' : report.revenue }  
 
-
-    reports_dict = {    'reports' : reports_list, 
+    reports_dict = {    'yearly_reports' : the_reports,
+                        'reports' : reports_list, 
                         't_revenue' : reports_revenue['revenue__sum'], 
                         't_partner_share' : reports_partner_share['partner_share__sum'],
                         't_contribution' : reports_contribution
     }
 
-
     #print to see what is inside variable
-    print(reports_revenue)
-    print(reports_partner_share)
 
     return render(request, 'kpis/index.html', context=reports_dict)
