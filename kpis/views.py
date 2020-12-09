880b5d99001bc1dd108ad1a4d28ac74e4aeaaf33
= <=>
2020-12-09 15:51:53 +0000

    update login page

--- kpis/views.py
+++ kpis/views.py
@@ -697,7 +697,7 @@ def login(request):
 
         if user is not None:
             auth.login(request, user)
-            return redirect("/")
+            return redirect("index")
         else:
             messages.info(request, 'Invalid credentials')
             return redirect("login")
